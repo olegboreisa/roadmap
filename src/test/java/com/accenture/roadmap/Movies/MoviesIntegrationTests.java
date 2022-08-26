fdca92fbc21fcebcee8cc82c40d3000112f6e628
olegboreisa <oboreisa@gmail.com>
2022-08-26 05:29:13 +0300

    Added Controller Integration Tests

--- src/test/java/com/accenture/roadmap/Movies/MoviesIntegrationTests.java
+++ src/test/java/com/accenture/roadmap/Movies/MoviesIntegrationTests.java
@@ -7,6 +7,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 
 @SpringBootTest(
@@ -14,21 +19,36 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
         webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class MoviesIntegrationTests {
 
-    private static final String DOMAIN_AND_PORT = "http://localhost:8080";
-    private static final String GET_ONE = "/api/movies/one?movieId=1";
-    private static final String POST = "/api/movies";
+    private static final String MOVIE_URL = "http://localhost:8080/api/movies";
+    private static final MovieForm FORM = new MovieForm(null, "Home Alone", 1.12);
 
     @Autowired
     private TestRestTemplate restTemplate;
 
-//    @Test
-//    public void shouldGetMovieById() {
-//        MovieDTO movie = restTemplate.getForObject(DOMAIN_AND_PORT + GET_ONE, MovieDTO.class);
-//    }
+    @Test
+    public void shouldGetMovieName() {
+        String URL = UriComponentsBuilder.fromUriString(MOVIE_URL + "/one")
+                .queryParam("movieId", "6")
+                .toUriString();
+
+        MovieDTO movie = restTemplate.getForObject(URL, MovieDTO.class);
+        assertNotNull(movie);
+        assertEquals("Titanic", movie.getName());
+    }
+
+    @Test
+    public void shouldAddNewMovie() {
+        ResponseEntity<Void> response = restTemplate.postForEntity(MOVIE_URL, FORM, Void.class);
+        assertEquals(200, response.getStatusCodeValue());
+    }
 //
 //    @Test
-//    public void shouldAddNewMovie() {
-//        MovieForm form = new MovieForm(null, "Why", 1.12);
-//        restTemplate.postForEntity(DOMAIN_AND_PORT + POST, form, Void.class);
+//    public void shouldDeleteMovie() {
+//        String URL = UriComponentsBuilder.fromUriString(MOVIE_URL + "/one")
+//                .queryParam("movieId", "6")
+//                .toUriString();
+//
+//        restTemplate.delete(MOVIE_URL, ResponseEntity.class);
 //    }
+
 }
